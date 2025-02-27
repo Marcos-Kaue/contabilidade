fd4b11338ddc8c9ad1780f50dacb8346505d7486
Marcos Kauê <marcoskrn19@gmail.com>
2025-02-27 12:52:35 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function abrirWhatssap () {
+    let name = document.getElementById('name').value;
+    let subject = document.getElementById('subject').value;
+    let message = document.getElementById('message').value;
+    let formulario = document.getElementById('formulario')
+
+    if(!name || !subject || !message){
+        alert('Preencha todos os campos');
+        return;
+    }
+
+    let mensagem = `Olá me chamo ${name}!%0A`;
+    mensagem += `Preciso da sua ajuda com: *${subject}*.%0A`;
+    mensagem += `Detalhes: ${message}.`;
+
+    const numeroWhatssap = "558899144483";
+    const url = `https://wa.me/${numeroWhatssap}/?text=${mensagem}`;
+    formulario.reset();
+    window.open(url,"_blank");
+
+}
+document.querySelectorAll(".faq-question").forEach(item => {
+    item.addEventListener("click", () => {
+        const answer = item.nextElementSibling;
+        answer.style.display = answer.style.display === "block" ? "none" : "block";
+    });
+});
+
+
+
+
